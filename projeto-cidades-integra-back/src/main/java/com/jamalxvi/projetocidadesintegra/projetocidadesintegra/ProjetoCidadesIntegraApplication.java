64cd3bb247e03c6ee5e290613cdf8a2e9e92b0bf
jamalxvi <henriquearantest@gmail.com>
2018-10-04 21:27:24 -0300

    First Project Commit: Initializing Back-end Structure

---
+++ projeto-cidades-integra-back/src/main/java/com/jamalxvi/projetocidadesintegra/projetocidadesintegra/ProjetoCidadesIntegraApplication.java
@@ -0,0 +1,12 @@
+package com.jamalxvi.projetocidadesintegra.projetocidadesintegra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCidadesIntegraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCidadesIntegraApplication.class, args);
+	}
+}
